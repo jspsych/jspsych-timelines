f4ad64388154503ae288702ae6616242e190be8a
Cherrie Chang <cchang@vassar.edu>
2025-03-31 13:55:44 -0400

    add stimuli files; move lswm out of nihtb-cb dir

--- packages/nih-toolbox-cognition-battery/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { JsPsych } from "jspsych";
-import listSortingWorkingMemoryTest, { timelineUnits as lswmTimelineUnits} from "./list-sorting-working-memory-test/index";
-
-export function createTimeline(jsPsych: JsPsych, {}) {}
-
-export const timelineUnits = {
-};
-
-export const utils = {};
