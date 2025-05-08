c0146985530b0f15692b1b754b31617e8d148e90
Cherrie Chang <cchang@vassar.edu>
2025-05-08 16:28:24 -0400

    add svg files and .js equivalents

--- packages/list-sorting-working-memory/src/index.ts
+++ packages/list-sorting-working-memory/src/index.ts
@@ -3,6 +3,8 @@ import jsPsychHtmlKeyboardResponse from "@jspsych/plugin-html-keyboard-response"
 import jsPsychPreload from "@jspsych/plugin-preload";
 import { JsPsych } from "jspsych";
 
+import { sesameSeedSvg } from "../assets/images/images-food/1-sesame_seed.js";
+
 // Constants
 const animalStimuli: Array<listSortingWorkingMemoryTestStimulusInfo> = [
   {
@@ -79,54 +81,39 @@ interface listSortingWorkingMemoryTestStimulusInfo {
   stimulus_audio: string;
 }
 
-// utils
-function preload(stimulusSet: Array<Array<listSortingWorkingMemoryTestStimulusInfo>>) {
-  return {
-    type: jsPsychPreload,
-    images: stimulusSet.flatMap((stimulus) =>
-      stimulus.map((stimulusInfo) => stimulusInfo.stimulus_image)
-    ),
-    audio: stimulusSet.flatMap((stimulus) =>
-      stimulus.map((stimulusInfo) => stimulusInfo.stimulus_audio)
-    ),
-  };
-}
-
-// timeline units
-function lswmTrial(
-  jsPsych: JsPsych,
-  options: {
-    stimulus_image?: string;
-    stimulus_audio?: string;
-  }
-) {
+function imageTrial(jsPsych, image) {
   return {
     type: jsPsychHtmlKeyboardResponse,
-    stimulus: options.stimulus_audio
-      ? options.stimulus_audio
-      : jsPsych.timelineVariable("stimulus_audio"),
-    prompt: `<img src="${
-      options.stimulus_image ? options.stimulus_image : jsPsych.timelineVariable("stimulus_image")
-    }" />`,
+    stimulus: image,
     choices: "NO_KEYS",
     trial_duration: 2000,
   };
 }
 
+function lswmTrial(jsPsych: JsPsych) {
+  return {
+    type: jsPsychAudioKeyboardResponse,
+    stimulus: jsPsych.timelineVariable("stimulus_audio"),
+    prompt: jsPsych.timelineVariable("stimulus_image"),
+    choices: "NO_KEYS",
+    trial_duration: 2000,
+  };
+}
+
+// timeline units
 function lswmTrialSequence(
   jsPsych: JsPsych,
   options: {
-    stimulus_set?: Array<listSortingWorkingMemoryTestStimulusInfo>;
-    sequence_length?: number;
+    stimulus_set: Array<listSortingWorkingMemoryTestStimulusInfo>;
+    sequence_length: number;
   }
 ) {
   return {
     timeline: [lswmTrial],
+    // TODO: Figure out whether this sampling should be engineered to sample as sparsely as possible
     timeline_variables: jsPsych.randomization.sampleWithoutReplacement(
-      options.stimulus_set ? options.stimulus_set : jsPsych.timelineVariable("stimulus_set"),
+      options.stimulus_set,
       options.sequence_length
-        ? options.sequence_length
-        : jsPsych.timelineVariable("sequence_length")
     ),
   };
 }
@@ -134,15 +121,16 @@ function lswmTrialSequence(
 function lswmSection(
   jsPsych: JsPsych,
   options: {
-    stimulus_set?: Array<Array<listSortingWorkingMemoryTestStimulusInfo>>;
-    sample_size_sequence?: Array<number>;
+    stimulus_set_list: Array<Array<listSortingWorkingMemoryTestStimulusInfo>>;
+    sample_size_sequence: Array<number>;
   }
 ) {
-  // Flatten stimulus_set
-  const stimulusSetFlat = options.stimulus_set.flatMap((stimulus) =>
+  // Flatten stimulus_set; include index as stimulus_set_id
+  const stimulusSetFlat = options.stimulus_set_list.flatMap((stimulus, list_idx) =>
     stimulus.map((stimulusInfo) => ({
       stimulus_image: stimulusInfo.stimulus_image,
       stimulus_audio: stimulusInfo.stimulus_audio,
+      stimulus_set_id: list_idx
     }))
   );
 
@@ -151,12 +139,8 @@ function lswmSection(
     options.sample_size_sequence = options.sample_size_sequence.map((size) =>
       Math.min(size, stimulusSetFlat.length)
     );
-  }
-  else {
-    options.sample_size_sequence = Array.from(
-      { length: stimulusSetFlat.length },
-      (_, i) => i + 1
-    );
+  } else {
+    options.sample_size_sequence = Array.from({ length: stimulusSetFlat.length }, (_, i) => i + 1);
   }
 
   let sectionTimeline = [];
@@ -207,18 +191,21 @@ export function createTimeline(
   }
 
   let timeline = [];
-  timeline.push(preload(options.stimulus_set));
+  // timeline.push(lswmTrial(jsPsych, {
+  //   stimulus_image: options.stimulus_set[0][0].stimulus_image,
+  //   stimulus_audio: options.stimulus_set[0][0].stimulus_audio,}));
 
   for (let i = 0; i < options.dimensions_sequence.length; i++) {
     const dimension = options.dimensions_sequence[i];
     timeline.push(
-      lswmSection(jsPsych, {
-        // Assume stimulus_set[0] is always first dimension, stimulus_set[1] is second dimension, etc.
-        stimulus_set: options.stimulus_set.toSpliced(
-          dimension,
-          options.stimulus_set.length - dimension
-        ),
-      })
+      imageTrial(jsPsych, sesameSeedSvg)
+      // lswmSection(jsPsych, {
+      //   // Assume stimulus_set[0] is always first dimension, stimulus_set[1] is second dimension, etc.
+      //   stimulus_set: options.stimulus_set.toSpliced(
+      //     dimension,
+      //     options.stimulus_set.length - dimension
+      //   ),
+      // })
     );
   }
   return timeline;
@@ -231,7 +218,6 @@ export const timelineUnits = {
 };
 
 export const utils = {
-  preload,
   animalStimuli,
   foodStimuli,
 };
